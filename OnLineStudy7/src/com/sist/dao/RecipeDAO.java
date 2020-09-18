b100c89ed58077a42e72497adaf50398bbed7b10
채제분 <@LAPTOP-G5H90GCB>
2020-09-18 12:00:00 +0900

    2222

--- OnLineStudy7/src/com/sist/dao/RecipeDAO.java
+++ OnLineStudy7/src/com/sist/dao/RecipeDAO.java
@@ -1,5 +1,41 @@
 package com.sist.dao;
-
+import java.io.*;
+/*
+ *   Collection 클래스  ==> 자료 구조 (데이터 저장하는 방법)
+ *   ========== 데이터를 저장하고 쉽게 저장이 가능하게 만든 클래스의 집합 
+ *     List ==============> 오라클
+ *      = 순서를 가지고 있다 
+ *      = 데이터가 중복이 가능하다 
+ *      = ***ArrayList,Vector,LinkedList
+ *     Set  =============> 사용빈도가 거의 없다 
+ *      = 순서가 없다 
+ *      = 데이터가 중복이 없다 
+ *      = HashSet , TreeSet  
+ *     Map
+ *      = 두개를 동시에 저장이 가능 (키(id),값) => request,response
+ *        ?no=10  ==> request.push("no",10)
+ *      = ***HashMap(hashtable의 단점을 보완),hashtable
+ *      = JSON(MongoDB)
+ *      = (id,"admin")
+ *      = 클래스 등록 , sql등록 
+ *      =================================== 저장 방법 
+ */
+/*
+ *   1. XML을 읽어와서 => 데이터 저장 
+ *                    ========= 셋팅 (getConnection(),disConnection())
+ *       SqlSessionFactory(Mybatis에서 지원하는 클래스)
+ *       =================
+ *       XML 파싱 
+ *   2. id를 설정해주고 sql을 실행이 가능하게 만든다 
+ */
 public class RecipeDAO {
-
+   
 }
+
+
+
+
+
+
+
+
