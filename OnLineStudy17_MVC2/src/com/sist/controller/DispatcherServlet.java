5c95d1e000e2cb384dc5fa623e3343c893a5feb9
채제분 <@LAPTOP-G5H90GCB>
2020-10-13 15:05:49 +0900

    111111

---
+++ OnLineStudy17_MVC2/src/com/sist/controller/DispatcherServlet.java
@@ -0,0 +1,22 @@
+package com.sist.controller;
+
+import java.io.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+// DispatcherServlet : Spring 실제 Controller명
+
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
